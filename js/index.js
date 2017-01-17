814fe2198c2bdc021f9c80f50ab2ef8a6fe912e2
tbae2 <thomas.bae0@gmail.com>
2017-01-17 11:41:10 -0500

    added delete function remove from tasklist as well as array

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@
 //store created tasks
 var tasks = [];
 
-
-
 //create object template with object constructor
 function taskObj(id,taskname, taskdescription, taskpriority){
       this.id = id;
@@ -12,7 +10,7 @@ function taskObj(id,taskname, taskdescription, taskpriority){
       this.priority = taskpriority;
 };
 
-
+//create task , update DOM, save to array (for future session storage)
 function createTask(){
     //grab input fields
     var inputName = document.getElementById("taskname").value;
@@ -21,7 +19,7 @@ function createTask(){
 
   // console.log(checkedItem);
     //get new ID for new Task
-    var taskNumber = tasks.length + 1;
+    var taskNumber = 'task' + (tasks.length + 1);
     //create Task
     var addedTask = new taskObj(taskNumber,inputName,inputDescription,checkedItem);
     //add new Task Object to array
@@ -32,7 +30,11 @@ function createTask(){
    var newTask = document.createElement("li");
    var taskContent = document.createTextNode(addedTask.taskname + " " + addedTask.description + " " + addedTask.priority);
    var removeTask = document.createElement("i");
-   //set class for icon
+    //set id for management later
+   newTask.setAttribute('id',taskNumber);
+    //set class for open task
+   newTask.setAttribute('class', 'opentask');
+    //set class for icon
    removeTask.setAttribute("class","material-icons");
    //create and append text node for icon <i> tag
    removeTask.appendChild(document.createTextNode("delete"));
@@ -43,21 +45,25 @@ function createTask(){
    //append newly created element to tasklist in DOM
    document.getElementById('tasklist').appendChild(newTask);
 
-
   clearform();
-  console.log(tasks);
-  console.log(inputName);
+  // console.log(tasks);
+  // console.log(inputName);
 };
 
 function removeTask(e){
-
-    console.log(e.target);
-    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    //assign targeted element to var
+    var delTarget = e.target;
+    //remove task, need to target parent node of parent node of delete button and then remove the child of the parent parent node
+    delTarget.parentNode.parentNode.removeChild(delTarget.parentNode);
+    //get index of targeted element to remove from array
+    var delTargetIndex = tasks.map(function(item){ return item.id }).indexOf(delTarget.parentNode.getAttribute('id'));
+    //remove from array
+    tasks.splice(delTargetIndex, 1);
 
 }
-
+//remove task
 document.getElementById('tasklist').addEventListener('click',removeTask,false);
-
+//submit task
 document.getElementById("tasksubmit").addEventListener('click',function(e){
   e.preventDefault();
   createTask();
