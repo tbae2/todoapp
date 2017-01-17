0670feb2e936162fc6fa5c4d7e177fc01f5070e3
tbae2 <thomas.bae0@gmail.com>
2017-01-16 22:42:20 -0500

    reworking object creation, added icon support and reference in html

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ var tasks = {};
 
 //create object template with object constructor
 function task(taskname, taskdescription, taskpriority){
+
       this.taskname = taskname;
       this.description = taskdescription;
       this.priority = taskpriority;
@@ -20,18 +21,23 @@ function createTask(){
 
   console.log(checkedItem);
 
-    var taskNumber = "task" + Object.keys(tasks).length + 1;
-
+    var taskNumber = Object.keys(tasks).length + 1;
+    console.log(taskNumber);
    tasks.taskNumber = new task(inputName,inputDescription,checkedItem);
 
    var newTask = document.createElement("li");
-   var taskContent = document.createTextNode(tasks.taskNumber.taskname+ " " + tasks.taskNumber.description + " " + tasks.taskNumber.priority);
+   var taskContent = document.createTextNode(tasks.taskNumber.taskname + " " + tasks.taskNumber.description + " " + tasks.taskNumber.priority);
+   var removeTask = document.createElement("i");
+   removeTask.setAttribute("class","material-icons");
+   removeTask.appendChild(document.createTextNode("delete"));
    newTask.appendChild(taskContent);
+   newTask.appendChild(removeTask);
 
    document.getElementById('tasklist').appendChild(newTask);
 
 
   clearform();
+  console.log(tasks);
   console.log(inputName);
 };
 
