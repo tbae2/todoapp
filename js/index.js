85f5eff1d0b419e2e200d59d5e5910b42fed5640
tbae2 <thomas.bae0@gmail.com>
2017-01-18 20:58:59 -0500

    implementing local storage usage

--- js/index.js
+++ js/index.js
@@ -10,6 +10,22 @@ function taskObj(id,taskname, taskdescription, taskpriority){
       this.priority = taskpriority;
 };
 
+window.onload = function(){
+   console.log(localStorage.length);
+   var keyName;
+        for(var i=0; i <= localStorage.length; i++){
+              keyName = localStorage.key(i);
+            if(keyName.substring(0,3) === 'task'){
+              console.log(localStorage.getItem(keyName));
+            }
+
+
+
+        }
+
+};
+
+
 //create task , update DOM, save to array (for future session storage)
 function createTask(){
     //grab input fields
@@ -17,42 +33,44 @@ function createTask(){
     var inputDescription = document.getElementById("taskdescription").value;
     var checkedItem = document.getElementById("inputtask")["priorities"].value;
 
-  // console.log(checkedItem);
     //get new ID for new Task
-    var taskNumber = 'task' + (tasks.length + 1);
+    var taskNumber = 'task' + localStorage.length;
     //create Task
     var addedTask = new taskObj(taskNumber,inputName,inputDescription,checkedItem);
     //add new Task Object to array
     tasks.push(addedTask);
   //  console.log(tasks);
-
+    localStorage.setItem(taskNumber,JSON.stringify(addedTask));
+    console.log(localStorage.length);
    //create new element, create text node, create icon element
-   var newTask = document.createElement("li");
-   var taskContent = document.createTextNode(addedTask.taskname + " " + addedTask.description + " " + addedTask.priority);
-   var removeTask = document.createElement("i");
-   var completeTask = document.createElement('i');
-    //set id for management later
-   newTask.setAttribute('id',taskNumber);
-    //set class for open task
-   newTask.setAttribute('class', 'opentask');
-    //set class for icon
-   completeTask.setAttribute('class', 'material-icons completetask');
-   removeTask.setAttribute("class","material-icons removetask");
-   //create and append text node for icon <i> tag
-   completeTask.appendChild(document.createTextNode('done'));
-   removeTask.appendChild(document.createTextNode("delete"));
-   //append task content and delete icon to created <li> element
-   newTask.appendChild(completeTask);
-   newTask.appendChild(taskContent);
-   newTask.appendChild(removeTask);
-
-
-   //append newly created element to tasklist in DOM
-   document.getElementById('tasklist').appendChild(newTask);
-
+   createDomTask(taskNumber,inputName,inputDescription,checkedItem);
   clearform();
   // console.log(tasks);
   // console.log(inputName);
+};
+
+function createDomTask(tasknumber,taskname,description,priority){
+  var newTask = document.createElement("li");
+  var taskContent = document.createTextNode(taskname + " " + description + " " + priority);
+  var removeTask = document.createElement("i");
+  var completeTask = document.createElement('i');
+   //set id for management later
+  newTask.setAttribute('id',tasknumber);
+   //set class for open task
+  newTask.setAttribute('class', 'opentask');
+   //set class for icon
+  completeTask.setAttribute('class', 'material-icons completetask');
+  removeTask.setAttribute("class","material-icons removetask");
+  //create and append text node for icon <i> tag
+  completeTask.appendChild(document.createTextNode('done'));
+  removeTask.appendChild(document.createTextNode("delete"));
+  //append task content and delete icon to created <li> element
+  newTask.appendChild(completeTask);
+  newTask.appendChild(taskContent);
+  newTask.appendChild(removeTask);
+  //append newly created element to tasklist in DOM
+  document.getElementById('tasklist').appendChild(newTask);
+
 };
 
 function updateTask(e){
@@ -64,16 +82,17 @@ function updateTask(e){
     if(updateTarget.getAttribute('class') === 'material-icons removetask'){
     //remove task, need to target parent node of parent node of delete button and then remove the child of the parent parent node
     updateParent.parentNode.removeChild(updateParent);
-    //get index of targeted element to remove from array
-    var updateTargetIndex = tasks.map(function(item){ return item.id }).indexOf(updateParent.getAttribute('id'));
+    //use id of list item to remove the corresponding item from local storage
+     localStorage.removeItem(updateParent.getAttribute('id'));
+    //var updateTargetIndex = tasks.map(function(item){ return item.id }).indexOf(updateParent.getAttribute('id'));
     //remove from array
-    tasks.splice(updateTargetIndex, 1);
+    //tasks.splice(updateTargetIndex, 1);
   } else if(updateTarget.getAttribute('class') === 'material-icons completetask'){
       updateParent.getAttribute('class') === 'opentask' ? updateParent.setAttribute('class', 'closedtask') : updateParent.setAttribute('class', 'opentask');
   }
 };
 
-//remove task
+//update task(remove,toggle complete or notd)
 document.getElementById('tasklist').addEventListener('click',updateTask,false);
 //submit task
 document.getElementById("tasksubmit").addEventListener('click',function(e){
